157e271fd58812eeeb9e78f6d4414a11f911b0f9
aklilubek <aklilubek2016@gmail.com>
2020-05-22 22:02:59 -0700

    first commit from eclipse

---
+++ classof March/src/AktestReposetery.java
@@ -0,0 +1,10 @@
+
+public class AktestReposetery {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello ClassOf March this is test from AK");
+	}
+
+}
