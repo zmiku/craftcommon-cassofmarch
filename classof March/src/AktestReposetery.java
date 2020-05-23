f46928e261a9cf750579d198fc32806f7bd04f7b
aklilubek <aklilubek2016@gmail.com>
2020-05-22 22:11:02 -0700

    AK eclipse PUSH

--- classof March/src/AktestReposetery.java
+++ classof March/src/AktestReposetery.java
@@ -4,7 +4,7 @@ public class AktestReposetery {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello ClassOf March this is test from AK");
+		System.out.println("Helloo ClassOf March this is test from AK");
 	}
 
 }
